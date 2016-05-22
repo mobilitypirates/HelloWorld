432c5d24e93e3eaed654235df669471768286318
Mobility Pirates <mobilitypirates@gmail.com>
2016-05-22 21:52:17 +0530

    test file added

--- app/src/main/java/com/practice/admin/helloworld/MainActivity.java
+++ app/src/main/java/com/practice/admin/helloworld/MainActivity.java
@@ -2,12 +2,23 @@ package com.practice.admin.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
+    Button b1;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.b);
+    }
+    public void click(View v)
+    {
+        Toast.makeText(MainActivity.this, "Button is Clicked : ", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/practice/admin/helloworld/Test.java
@@ -0,0 +1,7 @@
+package com.practice.admin.helloworld;
+
+/**
+ * Created by Admin on 5/22/2016.
+ */
+public class Test {
+}
